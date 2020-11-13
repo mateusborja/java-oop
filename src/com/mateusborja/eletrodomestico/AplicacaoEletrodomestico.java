3cb646840a21470bd04685735025942a66780922
mateus.borja <mateus.borja@gmail.com>
2020-11-13 11:36:49 -0400

    classe main eletrodomesticos

---
+++ src/com/mateusborja/eletrodomestico/AplicacaoEletrodomestico.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+package com.mateusborja.eletrodomestico;
+
+public class AplicacaoEletrodomestico {
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+	}
+
+}
