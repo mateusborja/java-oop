d8ec7ebbd978e114b61757f523677f802cdf8cf2
mateus.borja <mateus.borja@gmail.com>
2020-11-13 11:30:41 -0400

    classe abstrata eletrodomestico

---
+++ src/com/mateusborja/eletrodomestico/Eletrodomestico.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+package com.mateusborja.eletrodomestico;
+
+public abstract class Eletrodomestico {
+
+	private String nome;
+	private int voltagem;
+	private boolean ligado;
+
+	public abstract void ligar();
+
+	public abstract void desligar();
+
+	public Eletrodomestico(int voltagem, boolean ligado) {
+		this.setVoltagem(voltagem);
+		this.setLigado(ligado);
+	}
+
+	public Eletrodomestico(String nome, int voltagem, boolean ligado) {
+		this.setNome(nome);
+		this.setVoltagem(voltagem);
+		this.setLigado(ligado);
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setVoltagem(int votagem) {
+		this.voltagem = votagem;
+	}
+
+	public void setLigado(boolean ligado) {
+		if (ligado)
+			this.ligado = true;
+		else
+			this.ligado = false;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public double getVoltagem() {
+		return voltagem;
+	}
+
+	public boolean isLigado() {
+		return ligado;
+	}
+}
