201819ef54103b5eacf0981cb0d5e8b4318e5fa1
mateus.borja <mateus.borja@gmail.com>
2020-11-13 11:40:02 -0400

    classe main com teste padrao

--- src/com/mateusborja/eletrodomestico/AplicacaoEletrodomestico.java
+++ src/com/mateusborja/eletrodomestico/AplicacaoEletrodomestico.java
@@ -7,12 +7,27 @@
 package com.mateusborja.eletrodomestico;
 
 public class AplicacaoEletrodomestico {
-	
+
 	public static void main(String[] args) {
-		
-		
-		
-		
+
+		Geladeira geladeira1 = new Geladeira(110, true);
+
+		Radio radio1 = new Radio(110);
+
+		radio1.ligar();
+		radio1.setSintonia(99.1f);
+		radio1.setVolume(69);
+		System.out.print("O Radio está ");
+		System.out.print(radio1.isLigado() ? "ligado " : " desligado ");
+		System.out.println("e \"Sintonizado\" em " + radio1.getSintonia() + " com volume bem alto... volume = "
+				+ radio1.getVolume());
+
+		geladeira1.ligar();
+		geladeira1.setModelo("Brastemp");
+
+		System.out.print("\nA Geladeira " + geladeira1.getModelo());
+		System.out.println(geladeira1.isLigado() ? " está ligada." : "desligada.");
+
 	}
 
 }
