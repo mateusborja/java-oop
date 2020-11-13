13c2b343075d497e363ae2cdd150e210e33f8982
mateus.borja <mateus.borja@gmail.com>
2020-11-13 11:34:58 -0400

    java overloading, polimorfism, herança, abstração e encapsulamento

--- src/com/mateusborja/eletrodomestico/Radio.java
+++ src/com/mateusborja/eletrodomestico/Radio.java
@@ -7,23 +7,54 @@
 package com.mateusborja.eletrodomestico;
 
 public class Radio extends Eletrodomestico {
-	
 
-	public Radio(int voltagem, boolean ligado) {
-		super(voltagem, ligado);
-		// TODO Auto-generated constructor stub
+	public static final short AM = 1;
+	public static final short FM = 2;
+	private int banda;
+	private float sintonia;
+	private int volume;
+
+	public Radio(int voltagem) {
+		super(voltagem, true);
+		setBanda(Radio.FM);
+		setSintonia(0);
+		setVolume(0);
 	}
 
-	@Override
-	public void ligar() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
 	public void desligar() {
-		// TODO Auto-generated method stub
-		
+		super.setLigado(false);
+		setSintonia(0);
+		setVolume(0);
+	}
+
+	public void ligar() {
+		super.setLigado(true);
+		setSintonia(88.1f);
+		setVolume(25);
+	}
+
+	public int getBanda() {
+		return banda;
+	}
+
+	public void setBanda(int banda) {
+		this.banda = banda;
+	}
+
+	public float getSintonia() {
+		return sintonia;
+	}
+
+	public void setSintonia(float sintonia) {
+		this.sintonia = sintonia;
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+
+	public void setVolume(int volume) {
+		this.volume = volume;
 	}
 
 }
