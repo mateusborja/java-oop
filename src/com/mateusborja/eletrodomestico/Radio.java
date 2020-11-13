229a4f11acf7c9370faea54c4689eece4d8d7773
mateus.borja <mateus.borja@gmail.com>
2020-11-13 11:34:18 -0400

    classe radio

---
+++ src/com/mateusborja/eletrodomestico/Radio.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+package com.mateusborja.eletrodomestico;
+
+public class Radio extends Eletrodomestico {
+	
+
+	public Radio(int voltagem, boolean ligado) {
+		super(voltagem, ligado);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void ligar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void desligar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
