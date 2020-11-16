291d18c71b0ad226f6d45d0583754a6f1c232a95
Mateus Borja <mateus.borja@gmail.com>
2020-11-16 14:07:06 -0400

    aplicacao de calculo de impostos

---
+++ src/com/mateusborja/imposto/Orcamento.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+package com.mateusborja.imposto;
+
+public class Orcamento {
+
+	private double valor;
+
+	public Orcamento(double valor) {
+		this.valor = valor;
+
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+}
