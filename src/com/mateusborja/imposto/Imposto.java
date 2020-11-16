852f70bf550084dd724e23d08f3263c92cb8fe20
Mateus Borja <mateus.borja@gmail.com>
2020-11-16 14:37:37 -0400

    interface imposto que recebe um orcamento e retorna um double

---
+++ src/com/mateusborja/imposto/Imposto.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+package com.mateusborja.imposto;
+
+public interface Imposto {
+
+	double calcula(Orcamento orcamento);
+	
+}
