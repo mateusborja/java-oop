0552d0910ccf95b952fc5b994c601b24ee776796
Mateus Borja <mateus.borja@gmail.com>
2020-11-24 21:30:28 -0400

    Interface com metodos assinados

---
+++ src/com/mateusborja/animal/AnimalDomestico.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+package com.mateusborja.animal;
+
+public interface AnimalDomestico {
+
+	void levarVeterinario();
+
+	void alimentar();
+
+}
