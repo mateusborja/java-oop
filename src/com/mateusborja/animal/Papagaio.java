ccf045f4d86fad6957e4b0ae395e56bb9c4fc50e
Mateus Borja <mateus.borja@gmail.com>
2020-11-24 21:32:03 -0400

    classe Papagaio java oop, classes abstratas e interface

---
+++ src/com/mateusborja/animal/Papagaio.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+package com.mateusborja.animal;
+
+public class Papagaio extends Ave {
+
+	@Override
+	void voar() {
+		System.out.println("Passarinho está vonado.");
+		
+	}
+
+	@Override
+	void emitirSom() {
+		System.out.println("Piu Piu Piu Piu");
+		
+	}
+
+}
