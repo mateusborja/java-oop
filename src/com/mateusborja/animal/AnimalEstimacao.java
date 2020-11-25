217ed23ed6a8fbd631aa0f98b44a0ac7116f9724
Mateus Borja <mateus.borja@gmail.com>
2020-11-24 21:30:41 -0400

    Interface com metodos assinados

---
+++ src/com/mateusborja/animal/AnimalEstimacao.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+package com.mateusborja.animal;
+
+public interface AnimalEstimacao {
+	
+	void brincar();
+	void passear();
+
+}
