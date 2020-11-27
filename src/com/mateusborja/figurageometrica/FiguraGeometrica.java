ebd96f25e656eb0331d0ef2cffab8a2084349b59
Mateus Borja <mateus.borja@gmail.com>
2020-11-26 21:42:15 -0400

    super classe FiguraGeometrica

---
+++ src/com/mateusborja/figurageometrica/FiguraGeometrica.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+package com.mateusborja.figurageometrica;
+
+public class FiguraGeometrica {
+
+	private String nome;
+	private String cor;
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+}
