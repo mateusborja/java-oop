f74ab69e85376938bbe7c7712b03b05230212db2
Mateus Borja <mateus.borja@gmail.com>
2020-11-26 21:45:34 -0400

    classe abstrata Figura3D extende FiguraGeometrica e Implementa DimensaoVolumetrica

---
+++ src/com/mateusborja/figurageometrica/Figura3D.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+package com.mateusborja.figurageometrica;
+
+public abstract class Figura3D extends FiguraGeometrica implements DimensaoSuperficie, DimensaoVolumetrica {
+
+}
