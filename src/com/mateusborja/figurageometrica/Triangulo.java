f7b724b2c554943eb6755bc78c0536b147347ee7
Mateus Borja <mateus.borja@gmail.com>
2020-11-26 21:43:25 -0400

    classe triangulo implementa DimensaoSuperficie e extende classe Figura2D

---
+++ src/com/mateusborja/figurageometrica/Triangulo.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+package com.mateusborja.figurageometrica;
+
+public class Triangulo extends Figura2D implements DimensaoSuperficie {
+
+	private double base;
+	private double altura;
+
+	public double getBase() {
+		return base;
+	}
+
+	public void setBase(double base) {
+		this.base = base;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+
+	@Override
+	public double calcularArea() {
+		return (base * altura) / 2;
+	}
+
+}
